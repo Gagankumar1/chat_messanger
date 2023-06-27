2dac90cf7303fcc370bb28a03bbbc25a115fc4cb
Bhupendrakr12 <luckyaher47513@gmail.com>
2023-06-27 21:46:44 +0530

    real time chat app

---
+++ lib/shared/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:366120912104:web:741c041e03c58d1da6755d";
+  static String apiKey = "AIzaSyD6bAove5w0lZDItJ-1hLiF83Vq2tfokH4";
+  static String messagingSenderId = "366120912104";
+  static String projectId = "chat-app-1a23a";
+  final primaryColor = const Color(0xFF25D366) ;
+
+}
+//(0xFFee7b64)
